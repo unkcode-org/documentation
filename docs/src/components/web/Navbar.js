a3dd6e7671827d3356a09b8b9dea7fdaae638ce8
unknowns <unknowns0074@gmail.com>
2022-10-18 14:28:18 -0300

    Adding sidebar config

--- docs/src/components/web/Navbar.js
+++ docs/src/components/web/Navbar.js
@@ -1,84 +1,36 @@
 import React from "react";
 import NavbarLink from "./NavbarLink";
+import sidebarData from "config/sidebar.json";
 
 const Navbar = () => {
 	return (
 		<ul className="nav flex-column">
-			<li className="nav-item">
-				<NavbarLink to="/">Getting Started</NavbarLink>
-				<ul className="nav flex-column">
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_installation">
-							Installation
-						</a>
+			{sidebarData.map((section, key) => {
+				if (section.subMenu) {
+					return (
+						<li className="nav-item" key={key}>
+							<NavbarLink to={section.route}>{section.label}</NavbarLink>
+							<ul className="nav flex-column">
+								{section.subMenuData.map((subSection) => {
+									return (
+										<li className="nav-item">
+											<a className="nav-link" href={subSection.section}>
+												{subSection.label}
+											</a>
+										</li>
+									);
+								})}
+							</ul>
+						</li>
+					);
+				}
+
+				return (
+					<li className="nav-item" key={key}>
+						<NavbarLink to={section.route}>{section.label}</NavbarLink>
 					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_html_structure">
-							HTML Structure
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_sass">
-							Sass
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_color_schemes">
-							Color Schemes
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_theme_customization">
-							Customization
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_logo_settings">
-							Logo Settings
-						</a>
-					</li>
-				</ul>
-			</li>
-			<li className="nav-item">
-				<NavbarLink to="/security">Security Handlers</NavbarLink>
-				<ul className="nav flex-column">
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_header">
-							Header
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_navbar">
-							Navbar
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_sidebar">
-							Sidebar
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_footer">
-							Footer
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#idocs_box_layout">
-							Box Layout
-						</a>
-					</li>
-				</ul>
-			</li>
-			<li className="nav-item">
-				<a className="nav-link" href="#idocs_faq">
-					FAQ
-				</a>
-			</li>
-			<li className="nav-item">
-				<a className="nav-link" href="#idocs_support">
-					Support
-				</a>
-			</li>
+				);
+			})}
 		</ul>
 	);
 };
