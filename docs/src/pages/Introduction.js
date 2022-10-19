96515435f3894621e1d9326da6f42429b379e16c
unknowns <unknowns0074@gmail.com>
2022-10-18 21:17:55 -0300

    Installing a code syntax highlighter

--- docs/src/pages/Introduction.js
+++ docs/src/pages/Introduction.js
@@ -1,5 +1,7 @@
 import Main from "components/web/Main";
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Introduction = () => {
 	return (
@@ -7,11 +9,32 @@ const Introduction = () => {
 			<div id="installation">
 				<h2>Getting Started</h2>
 				<p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
-				<code>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-					<br />
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-				</code>
+				<SyntaxHighlighter showLineNumbers={true} wrapLongLines={true} language="html" style={tomorrow}>
+					{`<div id="setup">
+	<h2>Setting Up</h2>
+	<p> Hello </p>
+	<code>
+		testing
+		<br />
+		testing
+	</code>
+	<hr />
+</div>
+<div id="secretkey">
+	<h2>Generate App Key</h2>
+	<p> testing </p>
+	<code>
+		testing
+		<br />
+		testing
+	</code>
+	<code>
+		testing
+		<br />
+		testing 
+	</code>
+</div>`}
+				</SyntaxHighlighter>
 				<hr />
 			</div>
 			<div id="setup">
